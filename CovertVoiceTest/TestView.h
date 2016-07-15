ad9c9e447583d930d34793e07601369200d718e7
JustinYang <ityangjing@gmail.com>
2016-07-15 23:22:15 +0800

    update .gitignore

---
+++ CovertVoiceTest/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  CovertVoiceTest
+//
+//  Created by JustinYang on 6/12/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ CovertVoiceTest/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  CovertVoiceTest
+//
+//  Created by JustinYang on 6/12/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
